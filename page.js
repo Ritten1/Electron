7276f729d7aefbe180c4f7fa5fd4b49157db7c9a
lzp <luttin0521@163.com>
2023-02-07 23:11:11 +0800

    feat:learn api、process、file

---
+++ page.js
@@ -0,0 +1,7 @@
+function onLoad() {
+  document.getElementById('btn').addEventListener('click', getProcessInfo); //
+  let btnClick = document.getElementById('btn'); // btnClick.onclick =
+  getProcessInfo;
+}
+
+onLoad();
--- renderer.js
+++ renderer.js
@@ -5,3 +5,33 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const fs = require('fs');
+
+function getProcessInfo() {
+  //   console.log('getCPUsage', process.getCPUUsage());
+  console.log('env', process);
+}
+
+function onLoad() {
+  document.getElementById('btn').addEventListener('click', getProcessInfo); //
+  let btnClick = document.getElementById('btn'); // btnClick.onclick =
+  getProcessInfo;
+}
+
+onLoad();
+
+const dragWrapper = document.getElementById('drag');
+
+dragWrapper.addEventListener('drop', (e) => {
+  //   e.preventDefault();
+  const files = e.dataTransfer.files;
+  if (files && files.length > 0) {
+    const path = files[0].path;
+    console.log('path', path);
+    const content = fs.readFileSync(path);
+    console.log('content', content.toString());
+  }
+});
+dragWrapper.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
